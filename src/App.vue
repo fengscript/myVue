eefe2396154e64ea61b797e8c8eb6202f305b0b1
fengyanggang <fengyanggang@hotmail.com>
2017-01-06 17:58:01 +0800

    test

--- src/App.vue
+++ src/App.vue
@@ -1,1 +1,48 @@
-<h1>Hello Word</h1>
+<template>
+    <div class="container">
+        <input type="text" name="input-area" placeholder="To do list" value="" v-bind="input">
+        <ul class="todoList">
+            <li class="items">111</li>
+        </ul>
+        <span>{{a}}</span>
+        <img src="./assets/close.png" alt="">
+    </div>
+</template>
+<style>
+    .container input {
+        margin: 50px auto;
+        width: 40%;
+        height: 60px;
+        display: block;
+        font-size: 30px;
+        text-indent: 12px;
+    }
+    
+    .container img {
+        width: 32px;
+        max-width: 100%
+    }
+    
+    .todoList {
+        margin: 50px auto;
+        width: 40%;
+        height: 60px;
+        display: block;
+        font-size: 30px;
+        text-indent: 12px;
+    }
+</style>
+<script>
+    export default{
+        data(){
+            return{
+                el:'.container',
+                input(){
+                    var a = this.value();
+                    document.write(a);
+                }
+
+            }
+        }
+    }
+</script>
